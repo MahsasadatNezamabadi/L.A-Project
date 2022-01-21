07962071d07924ff51dcb1b3d112dbe0f75d2494
clara <clara.siepmann@stud.uni-due.de>
2022-01-21 17:42:33 +0100

    connected drop down & sliders for ml part

--- apps/mlPart.py
+++ apps/mlPart.py
@@ -16,13 +16,19 @@ label_school=[]
 for l in schools:
     dict_label={"label":l,"value":l}
     label_school.append(dict_label)
+first_school=label_school[0]["value"]
+print(first_school)
 
 layout=html.Div([
     html.H1("Second Page"),
     html.Span("Pick a school to see the prediction"),
     dcc.Dropdown(id="school",
                  options=label_school,
-                 searchable=True),
+                 searchable=True,
+                 clearable=False,
+                 value=first_school
+                 ),
+    html.Div([
     html.Span("Pick the number of Features for the Machine Learning Model"),
     html.Span("Number of trees"),
     dcc.Slider(
@@ -79,9 +85,29 @@ layout=html.Div([
         max=data["bins"].max(),
         step=1,
         value=10,
-    ),
-    html.Div(id='slider-output')
+    )]),
+    html.Div(id='slider-output'),
+    html.Div(id="school_value")
 ])
+#@app.callback(Output(component_id="school_value"), Input(component_id="school"))
+@app.callback([Output(component_id="trees",component_property="value"),
+               Output(component_id="crashes",component_property="value"),
+               Output(component_id="pc",component_property="value"),
+               Output(component_id="shootings",component_property="value"),
+               Output(component_id="arrests",component_property="value"),
+               Output(component_id="programs",component_property="value"),
+               Output(component_id="bins",component_property="value")],
+              Input(component_id= "school", component_property="value"))
+def get_sliderValues(school):
+    df=data[data["school_name"]==school]
+    trees=df["trees"].to_list()[0]
+    crashes=df["crashes"].to_list()[0]
+    pc=df["pc"].to_list()[0]
+    shootings=df["shootings"].to_list()[0]
+    arrests=df["arrests"].to_list()[0]
+    programms= df["programs"].to_list()[0]
+    bins= df["bins"].to_list()[0]
+    return trees,crashes,pc,shootings,arrests,programms,bins
 
 @app.callback(Output(component_id="slider-output",component_property="children"),[
     Input(component_id="trees",component_property="value"),Input(component_id="crashes",component_property="value"),
